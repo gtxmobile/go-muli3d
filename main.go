1358e98d30786a1e3b8b3696e9534042d9bb2598
unknown <cydwillis@gmail.com>
2018-10-30 22:38:18 +0800

    jia

--- main.go
+++ main.go
@@ -161,25 +161,99 @@ func matrix_set_scale(m *Matrix_t, x float64,y float64, z float64){
 	m.M[2][2] = z
 
 }
-//旋转
+//从四元数构造旋转矩阵
 func matrix_set_rotate(m *Matrix_t, x float64,y float64, z float64, theta float64){
 	qsin := math.Sin(theta * 0.5)
 	qcos := math.Cos(theta * 0.5)
 	vec := Vector_t{x,y,z,1.0	}
 	w := qcos
+	vector_normalize(&vec)
 	x = vec.X * qsin
 	y = vec.Y * qsin
 	z = vec.Z * qsin
+
+	x2 := x * x
+	y2 := y * y
+	z2 := z * z
+
+	xy := x * y
+	xz := x * z
+	yz := y * z
+
+	wx := w * x
+	wy := w * y
+	wz := w * z
+	m.M[0][0] = 1 - 2*y2 - 2*z2
+	m.M[1][0] = 2*xy - 2*wz
+	m.M[2][0] = 2*xz + 2*wy
+
+	m.M[0][1] = 2*xy + 2*wz
+	m.M[1][1] = 1-2*x2 -2*z2
+	m.M[2][1] = 2*yz - 2*wx
+
+	m.M[0][2] = 2*xz - 2*wy
+	m.M[1][2] = 2*yz + 2*wx
+	m.M[2][2] = 1-2*x2 - 2*y2
+
+	m.M[0][3] , m.M[1][3],m.M[2][3] = 0,0,0
+	m.M[3][0] , m.M[3][1],m.M[3][2] = 0,0,0
+	m.M[3][3] = 1
 	}
 
+func matrix_set_lookat(m *Matrix_t,eye *Vector_t,at *Vector_t,up *Vector_t) {
+	var xaxis,yaxis,zaxis Vector_t
+	vector_sub(&zaxis,at,eye)
+	vector_normalize(&zaxis)
+	vector_crossproduct(&xaxis,up,&zaxis)
+	vector_normalize(&xaxis)
+	vector_crossproduct(&yaxis,&zaxis,&xaxis)
 
-	//坐标变换
+	m.M[0][0] = xaxis.X
+	m.M[1][0] = xaxis.Y
+	m.M[2][0] = xaxis.Z
+	m.M[3][0] = -vector_dotproduct(&xaxis,eye)
 
-type transform_t struct {
-	word 	Matrix_t
+	m.M[0][1] = yaxis.X
+	m.M[1][1] = yaxis.Y
+	m.M[2][1] = yaxis.Z
+	m.M[3][1] = -vector_dotproduct(&yaxis,eye)
+
+	m.M[0][2] = zaxis.X
+	m.M[1][2] = zaxis.Y
+	m.M[2][2] = zaxis.Z
+	m.M[3][2] = -vector_dotproduct(&zaxis,eye)
+
+	m.M[0][3],m.M[1][3],m.M[2][3] = 0,0,0
+	m.M[3][3] = 1
+
+}
+
+func matrix_set_perspective(m *Matrix_t,fovy float64,aspect float64,zn float64,zf float64){
+	fax := 1/math.Tan(fovy * 0.5)
+	matrix_set_zero(m)
+	m.M[0][0] = fax / aspect
+	m.M[1][1] = fax
+	m.M[2][2] = zf/(zf - zn)
+	m.M[3][2] = - zn * zf /(zf-zn)
+	m.M[2][3] = 1
+
+}
+
+//坐标变换
+type Transform_t struct {
+	world 	Matrix_t
 	view	Matrix_t
 	projection 	Matrix_t
 	transform 	Matrix_t
 	w	float64
 	h	float64
+}
+
+//矩阵更新
+
+func transform_update(ts *Transform_t){
+	var m Matrix_t
+	matrix_mul(&m, &ts.world,&ts.view)
+	matrix_mul(&ts.transform,&m,&ts.projection)
+
 }
