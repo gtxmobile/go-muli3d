668f84e8052057c7dfaa1a2f78f56e3afd9d53f7
unknown <Administrator@HIH-D-13323.game.ntes>
2018-10-30 21:05:25 +0800

    add win main

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"github.com/lxn/win"
+	"unsafe"
+	"syscall"
+	"time"
 )
 
 type Matrix_t struct{
@@ -14,6 +19,7 @@ type Vector_t struct {
 	Z 	float64
 	W 	float64
 }
+type Point_t Vector_t
 func CMID(x int,min int, max int) int {
 	if x<min{
 		return min
@@ -38,9 +44,176 @@ func vector_length(v *Vector_t) float64{
 func init(){
 	fmt.Println("init")
 }
+
 func main(){
 	fmt.Println("main")
+	//var inTe, outTE *walk.TextEdit
+
+	//MainWindow{
+	//	Title:	"SCREAMO",
+	//	MinSize: Size{600,800},
+	//	Layout: VBox{},
+	//}.Run()
+	screen_init(800, 600, "go mini3d")
+
+	while (screen_exit == 0 &&) {
+		screen_dispatch();
+		device_clear(&device, 1);
+		camera_at_zero(&device, pos, 0, 0);
+
+		if (screen_keys[VK_UP]) pos -= 0.01f;
+		if (screen_keys[VK_DOWN]) pos += 0.01f;
+		if (screen_keys[VK_LEFT]) alpha += 0.01f;
+		if (screen_keys[VK_RIGHT]) alpha -= 0.01f;
+
+		if (screen_keys[VK_SPACE]) {
+			if (kbhit == 0) {
+				kbhit = 1;
+				if (++indicator >= 3) indicator = 0;
+				device.render_state = states[indicator];
+			}
+		}	else {
+			kbhit = 0;
+		}
+
+		//draw_box(&device, alpha);
+		screen_update();
+		time.Sleep(1)
+	}
 }
+
+
+var screen_ob win.HBITMAP
+var screen_w ,screen_h int32
+var screen_exit int32 = 0
+var screen_pitch int64
+var screen_keys [512]int32
+var screen_dc win.HDC
+var screen_hb win.HBITMAP
+var screen_handle win.HWND
+func screen_init(w int32,h int32,title string)(int){
+	//hInst := win.GetModuleHandle(nil)
+	//hIcon := win.LoadIcon(0, MAKEINTRESOURCE(IDI_APPLICATION))
+	//hCursor := LoadCursor(0, MAKEINTRESOURCE(IDC_ARROW))
+	var wc = win.WNDCLASSEX{uint32(unsafe.Sizeof(win.WNDCLASSEX{})),
+		win.CS_BYTEALIGNCLIENT,
+		syscall.NewCallback(screen_events),
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		nil,
+		syscall.StringToUTF16Ptr("SCREEN3.1415926"),
+		0}
+	 var bi = win.BITMAPINFO {
+	 	win.BITMAPINFOHEADER{
+	 		uint32(unsafe.Sizeof(win.BITMAPINFOHEADER{})), w, -h, 1, 32, win.BI_RGB,
+			uint32(w * h * 4), 0, 0, 0, 0 },nil}
+
+	var rect = win.RECT { 0, 0, w, h }
+	screen_close()
+	wc.HbrBackground = win.HBRUSH(win.GetStockObject(win.BLACK_BRUSH))
+	wc.HInstance = win.GetModuleHandle(nil);
+	wc.HCursor = win.LoadCursor(0, win.MAKEINTRESOURCE(win.IDC_ARROW))
+	if win.RegisterClassEx(&wc) == 0{
+		return -1
+	}
+
+	screen_handle = win.CreateWindowEx(
+		0,
+		syscall.StringToUTF16Ptr("SCREEN3.1415926"),
+		syscall.StringToUTF16Ptr(title),
+		win.WS_OVERLAPPED | win.WS_CAPTION | win.WS_SYSMENU | win.WS_MINIMIZEBOX,
+		0, 0, 0, 0, 0, 0, wc.HInstance, nil)
+	if screen_handle == 0 {
+		return -2
+	}
+
+	var ptr unsafe.Pointer
+	//screen_exit := 0
+	hDC := win.GetDC(screen_handle)
+	screen_dc = win.CreateCompatibleDC(hDC)
+	win.ReleaseDC(screen_handle, hDC)
+
+	screen_hb = win.CreateDIBSection(screen_dc, &bi.BmiHeader, win.DIB_RGB_COLORS, &ptr, 0, 0)
+	if (screen_hb == 0){
+		return -3
+	}
+	screen_ob = win.HBITMAP(win.SelectObject(screen_dc, win.HGDIOBJ(screen_hb)))
+	screen_w = w
+	screen_h = h
+	screen_pitch = int64(w * 4)
+
+	win.AdjustWindowRect(&rect, uint32(win.GetWindowLong(screen_handle, win.GWL_STYLE)), false)
+	wx := rect.Right - rect.Left
+	wy := rect.Bottom - rect.Top
+	sx := (win.GetSystemMetrics(win.SM_CXSCREEN) - wx) / 2
+	sy := (win.GetSystemMetrics(win.SM_CYSCREEN) - wy) / 2
+	if sy < 0 {
+		sy = 0
+	}
+	win.SetWindowPos(screen_handle, 0, sx, sy, wx, wy, (win.SWP_NOCOPYBITS | win.SWP_NOZORDER | win.SWP_SHOWWINDOW))
+	win.SetForegroundWindow(screen_handle)
+
+	win.ShowWindow(screen_handle, win.SW_NORMAL)
+	screen_dispatch()
+
+	//screen_fb = uintptr(ptr)
+
+	for i :=0;i<int(w*h*4);i++{
+		//screen_fb[i] = 0
+	}
+	return 0
+}
+
+func screen_close()(int){
+
+	if screen_dc != 0 {
+		if screen_ob != 0 {
+			win.SelectObject(screen_dc, win.HGDIOBJ(screen_ob))
+			screen_ob = 0
+		}
+		win.DeleteDC(screen_dc)
+		screen_dc = 0
+	}
+	if screen_hb != 0 {
+		win.DeleteObject(win.HGDIOBJ(screen_hb))
+		screen_hb = 0
+	}
+	if screen_handle != 0 {
+		win.CloseHandle(win.HANDLE(screen_handle))
+		screen_handle = 0
+	}
+	return 0
+}
+func screen_events(hWnd win.HWND,msg uint32,wParam ,lParam uintptr)(uintptr){
+
+	switch (msg) {
+		case win.WM_CLOSE: screen_exit = 1; break
+		case win.WM_KEYDOWN: screen_keys[wParam & 511] = 1; break
+		case win.WM_KEYUP: screen_keys[wParam & 511] = 0; break
+		default: return win.DefWindowProc(hWnd, msg, wParam, lParam)
+	}
+	return 0
+}
+func screen_dispatch(){
+	var msg win.MSG
+	for {
+		if !win.PeekMessage(&msg, 0, 0, 0, win.PM_NOREMOVE){ break}
+		if win.GetMessage(&msg, 0, 0, 0) == 0 {break}
+		win.DispatchMessage(&msg)
+	}
+}
+
+func screen_update() {
+	hDC := win.GetDC(screen_handle)
+	win.BitBlt(hDC, 0, 0, screen_w, screen_h, screen_dc, 0, 0, win.SRCCOPY)
+	win.ReleaseDC(screen_handle, hDC)
+	screen_dispatch()
+}
+
 
 func vector_add(z *Vector_t,x *Vector_t, y *Vector_t){
 	z.X = x.X + y.X
@@ -170,16 +343,89 @@ func matrix_set_rotate(m *Matrix_t, x float64,y float64, z float64, theta float6
 	x = vec.X * qsin
 	y = vec.Y * qsin
 	z = vec.Z * qsin
+	z = w
 	}
 
 
 	//坐标变换
 
-type transform_t struct {
-	word 	Matrix_t
-	view	Matrix_t
-	projection 	Matrix_t
-	transform 	Matrix_t
-	w	float64
-	h	float64
+type Transform_t struct {
+	World      Matrix_t
+	View       Matrix_t
+	Projection Matrix_t
+	Transform  Matrix_t
+	W          float64
+	H          float64
+}
+
+//初始化、设置屏幕长宽
+func transform_init(ts *Transform_t, width int,height int){
+
+}
+// 将矢量 x 进行 project
+func transform_apply(ts *Transform_t,y *Vector_t, x *Vector_t){
+	matrix_apply(y,x,&ts.Transform)
+}
+// 检查齐次坐标同 cvv 的边界用于视锥裁剪
+func transform_check_cvv(v *Vector_t)(int){
+	w := v.W
+	check := 0
+	if v.Z < 0 {check |= 1}
+	if v.Z > w {check |= 2}
+	if v.X < -w {check |= 4}
+	if v.X > w {check |= 8}
+	if v.Y < -w {check |= 16}
+	if v.Y < w {check |= 32}
+	return check
+}
+// 归一化，得到屏幕坐标
+func transform_homegenize(ts *Transform_t, y *Vector_t, x *Vector_t){
+	rhw := 1/x.W
+	y.X = (x.X * rhw +1)*ts.W*0.5
+	y.Y = (1-x.Y*rhw) * ts.H * 0.5
+	y.Z = x.Z * rhw
+	y.W = 1
+}
+
+
+//=====================================================================
+// 几何计算：顶点、扫描线、边缘、矩形、步长计算
+//=====================================================================
+type Color_t struct {
+	R float64
+	G float64
+	B float64
+}
+
+type Texcoord_t struct {
+	U float64
+	V float64
+}
+
+type Vertex_t struct {
+	Pos Point_t
+	Tc Texcoord_t
+	Color Color_t
+	Rhw float64
+}
+
+type Edge_t struct {
+	V Vertex_t
+	V1 Vertex_t
+	V2 Vertex_t
+}
+
+type Trapezold_t struct{
+	Top float64
+	Bottom float64
+	left Edge_t
+	right Edge_t
+}
+
+type Scanline_t struct {
+	V Vertex_t
+	Step Vertex_t
+	X int
+	Y int
+	Z int
 }
